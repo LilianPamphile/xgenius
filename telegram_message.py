ca2fc6139123ee44dfaf26456ba29e949aec688e
LilianPamphile <90441506+LilianPamphile@users.noreply.github.com>
2025-08-16 21:08:01 +0200

    Update telegram_message.py

--- telegram_message.py
+++ telegram_message.py
@@ -1,15 +1,15 @@
-import os
 import requests
 
-def send_telegram_message(bot_token: str, chat_id: str, message: str):
+def send_telegram_message(bot_token: str, chat_id: str, message: str, parse_mode: str = None):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {
         "chat_id": chat_id,
         "text": message,
-        "parse_mode": "HTML",
-        "disable_web_page_preview": True
+        "disable_web_page_preview": True,
     }
-    r = requests.post(url, data=payload)
+    if parse_mode:
+        payload["parse_mode"] = parse_mode
+    r = requests.post(url, data=payload, timeout=20)
     if r.status_code == 200:
         print("📲 Message envoyé sur Telegram.")
     else:
